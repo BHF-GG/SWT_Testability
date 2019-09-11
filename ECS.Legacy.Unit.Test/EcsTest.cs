7b8e718958232a82f0c6bcd746ae40e1049ae3e2
Frands Andersen <frands1995@gmail.com>
2019-09-11 14:33:54 +0200

    Test opsætning tilføjet

---
+++ ECS.Legacy.Unit.Test/EcsTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECS.Legacy.classes;
+
+namespace ECS.Legacy.Unit.Test
+{
+    public class EcsTest
+    {
+        private ECS uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new ECS(10,new FakeTempSensor(), new FakeHeater());
+        }
+    }
+}
--- ECS.Legacy.Unit.Test/FakeTempSensor.cs
+++ ECS.Legacy.Unit.Test/FakeTempSensor.cs
@@ -9,5 +9,16 @@ namespace ECS.Legacy.classes
 {
     class FakeTempSensor : ITempSensor
     {
+        private Random gen = new Random();
+
+        public int GetTemp()
+        {
+            return gen.Next(-5, 45);
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
     }
 }
